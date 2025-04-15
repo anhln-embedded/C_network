efdb6a65d9ee51e7bb9dad58cf686d6af3792a41
anhln-embedded <anhln.embedded@gmail.com>
2025-04-15 09:25:51 +0700

    gui is ok

--- get_route_ipv4/src/gui.c
+++ get_route_ipv4/src/gui.c
@@ -3,41 +3,63 @@
 static route_ipv4_node_t *g_phead = NULL;
 
 static void gui_display_header(void);
-static void gui_display_route(route_ipv4_node_t *node);
+static void gui_display_route(route_ipv4_node_t *node, int index);
 static void gui_display_footer(void);
+static void gui_printCentered(const char *str, int width);
 
 void gui_init(void)
 {
     route_ipv4_init();
     route_ipv4_get(&g_phead);
 }
+
 void gui_display(void)
 {
     gui_display_header();
     route_ipv4_node_t *current = g_phead;
+    int index = 1; 
     while (current != NULL)
     {
-        gui_display_route(current);
+        gui_display_route(current, index);
         current = current->next;
+        index++;
+        gui_display_footer();
     }
-    gui_display_footer();
 }
 
 void gui_cleanup(void)
 {
+    route_ipv4_cleanup(); 
 }
 
 static void gui_display_header(void)
 {
+    printf("+-----+-----------------+-----------------+-----------------+-------------+\n");
+    printf("| IDX |   Destination   |     Gateway     |      Mask       | Is Default  |\n");
+    printf("+-----+-----------------+-----------------+-----------------+-------------+\n");
 }
-static void gui_display_route(route_ipv4_node_t *node)
+
+static void gui_display_route(route_ipv4_node_t *node, int index)
 {
-    printf("Destination: %s\n", node->route.destination);
-    printf("Gateway: %s\n", node->route.gateway);
-    printf("Mask: %s\n", node->route.mask);
-    printf("Is Default: %s\n", node->route.is_default ? "Yes" : "No");
-    printf("\n");
+    printf("| %3d | ", index);
+    gui_printCentered(node->route.destination, 15);
+    printf(" | ");
+    gui_printCentered(node->route.gateway, 15);
+    printf(" | ");
+    gui_printCentered(node->route.mask, 15);
+    printf(" | ");
+    gui_printCentered(node->route.is_default ? "Yes" : "No", 11);
+    printf(" |\n");
 }
+
 static void gui_display_footer(void)
 {
+    printf("+-----+-----------------+-----------------+-----------------+-------------+\n");
+}
+
+static void gui_printCentered(const char *str, int width)
+{
+    int len = strlen(str);
+    int padding = (width - len) / 2;
+    printf("%*s%s%*s", padding, "", str, padding + (width - len) % 2, "");
 }
