08a60e72e918e0e82a433d14976ed0a9bcb30ae9
anhln-embedded <anhln.embedded@gmail.com>
2025-04-15 09:50:28 +0700

    done

--- get_route_ipv4/src/main.c
+++ get_route_ipv4/src/main.c
@@ -1,8 +1,13 @@
 #include "gui.h"
+#include "log.h"
 
 
-int main()
+int main(int argc, char *argv[])
 {   
+    if (argc > 1) {
+        log_set_level(atoi(argv[1]));
+        printf("Log level set to %d\n", atoi(argv[1]));
+    }
     gui_init();
     gui_display();
     return 0;
